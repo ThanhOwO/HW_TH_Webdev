129b924aef52b9871f9564e153933397f186af11
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2022-11-13 09:40:34 +0700

    HW 3

--- Homework/Week2/Homework3/-template-processor.js
+++ Homework/Week2/Homework3/-template-processor.js
@@ -1,13 +1,18 @@
-var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
 
-var dateTemplate = new TemplateProcessor(template);
 
-var dictionary = {month: 'July', day: '1', year: '2016'};
 
-var str = dateTemplate.fillIn(dictionary);
-assert(str === 'My favorite month is July but not the day 1 or the year 2016');
+class TemplateProcessor{
+    constructor(template) {
+        this.template = template;
+    }
 
-//Case: property doesn't exist in dictionary
-var dictionary2 = {day: '1', year: '2016'};
-var str = dateTemplate.fillIn(dictionary2);
-assert(str === 'My favorite month is but not the day 1 or the year 2016')
+    fillIn(dictionary) {
+        var temp = this.template;
+        for (var key in dictionary) {
+            var re = new RegExp('\\{\\{' + key + '\\}\\}');
+            temp = temp.replace(re, dictionary[key]);
+        }
+        temp = temp.replace(new RegExp('\\{\\{\\w+\\}\\}', "g"), "");
+        return temp;
+    }
+}
--- Homework/Week2/Homework3/make-multi-filter.js
+++ Homework/Week2/Homework3/make-multi-filter.js
@@ -1,39 +1,16 @@
-
-var arrayFilterer1 = MakeMultiFilter([1, 2, 3]);
-
-// Call arrayFilterer1 (with a callback function) to filter out all the numbers
-// not equal to 2.
-arrayFilterer1(function (elem) {
- return elem !== 2;
-}, function (currentArray) {
- // 'this' within the callback function should refer to originalArray which is [1, 2, 3]
- console.log(this); 
- console.log(currentArray); 
-});
-
-// Call arrayFilterer1 (without a callback function) to filter out all the
-// elements not equal to 3.
-arrayFilterer1(function (elem) {
- return elem !== 3;
-});
-
-// Calling arrayFilterer1 with no filterCriteria should return the currentArray.
-var currentArray = arrayFilterer1();
-console.log('currentArray', currentArray);
-
-// Since arrayFilterer returns itself, calls can be chained
-function filterTwos(elem) { return elem !== 2; }
-
-function filterThrees(elem) { return elem !== 3; }
-
-var arrayFilterer2 = MakeMultiFilter([1, 2, 3]);
-
-var currentArray2 = arrayFilterer2(filterTwos)(filterThrees)();
-
-console.log('currentArray2', currentArray2); 
-
-// Multiple active filters at the same time
-var arrayFilterer3 = MakeMultiFilter([1, 2, 3]);
-var arrayFilterer4 = MakeMultiFilter([4, 5, 6]);
-console.log(arrayFilterer3(filterTwos)()); 
-console.log(arrayFilterer4(filterThrees)()); 
+'use strict';
+function MakeMultiFilter(originalArray) {
+    var currentArray = originalArray;
+    function arrayFilter (filterCriteria, callback) {
+        if (typeof filterCriteria !== "function") {
+            return currentArray;
+        }        
+        
+        currentArray = currentArray.filter(filterCriteria);
+        if (typeof callback === "function") {
+            callback.call(originalArray, currentArray);
+        }
+        return arrayFilter;
+    }
+    return arrayFilter;
+}
